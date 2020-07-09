d6938ac704c7bdd4855a91498ad43fb7f7a462d6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-08 22:06:52 -0400

    Add favorite pet indicator spec'

---
+++ spec/features/pets/favorite_indicator_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'favorite pet indicator' do
+  it 'can display a count of pets in favorites list' do
+    shelter = Shelter.create!(name: 'Old Dog Haven',
+                               address: '166 Main St',
+                               city: 'Denver',
+                               state: 'CO',
+                               zip: '80208')
+
+    pet = Pet.create!(image: 'https://i.ytimg.com/vi/2xZsXlSj-ts/maxresdefault.jpg',
+                     name: 'Maggie',
+                     description: 'A thoughtful sentient being',
+                     age: '2 years',
+                     sex: 'female',
+                     shelter: shelter)
+
+    visit '/'
+
+    expect(page).to have_content("Favorite Pets: 0")
+
+    visit '/pets'
+
+    click_on 'Maggie'
+    click_on 'Add Pet to Favorites'
+
+    expect(page).to have_content("Favorite Pets: 1")
+  end
+end
