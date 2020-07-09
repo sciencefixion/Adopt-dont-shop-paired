fcf1497f4d9307728b94a474df0debf68c016fe6
Rion Hutsell <st.gish@gmail.com>
2020-07-09 15:48:18 -0600

    Add Pets to Favorites index page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,17 @@
 class FavoritesController < ApplicationController
   def index
+    @favorites = session[:favorite_pets]
+
   end
 
   def update
     pet = Pet.find(params[:pet_id])
-    session[:counter] ||= 0
-    session[:counter] += 1
+    pet_id_str = pet.id.to_s
+    session[:favorite_pets] ||= Hash.new(0)
+    session[:favorite_pets][pet_id_str] ||= 0
+    session[:favorite_pets][pet_id_str] = session[:favorite_pets][pet_id_str] + 1
     flash[:notice] = "You added #{pet.name} to your favorites!"
     redirect_to '/pets'
   end
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,11 @@
-# class Favorite
-#   attr_reader :contents
-#
-#   def initialize(initial_contents)
-#     @contents = initial_contents
-#   end
-# end
+class Favorite
+  attr_reader :contents
+
+  def initialize(initial_contents)
+    @contents = initial_contents
+  end
+
+  def total_count
+    @contents.values.sum
+  end
+end
