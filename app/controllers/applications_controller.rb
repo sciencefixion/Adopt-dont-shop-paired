60e199f5d1a2ca6d8d9e2a557fc4a9e87fd59660
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-11 22:36:49 -0400

    Add create method to application controller)

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,13 @@ class ApplicationsController < ApplicationController
     pet_ids = @favorites.contents.keys.map {|id| id.to_i }
     @pets = pet_ids.map { |id| Pet.find(id) }
   end
+
+  def create
+    application = Application.create(application_params)
+  end
+
+  private
+  def application_params
+    params.permit(:pets, :name, :address, :city, :state, :zip, :phone_number, :description)
+  end
 end
