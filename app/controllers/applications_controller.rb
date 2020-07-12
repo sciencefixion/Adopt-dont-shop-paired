c4ce071289a4a8439093f705257a3c0468dfb3e3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-11 20:34:32 -0400

    Add html to applications view page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationsController < ApplicationController
   def new
-    
+    @favorites = Favorite.new(session[:favorite_pets])
+    pet_ids = @favorites.contents.keys.map {|id| id.to_i }
+    @pets = pet_ids.map { |id| Pet.find(id) }
   end
 end
