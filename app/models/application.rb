cde0f000658f9b02acc2f6e038983e7935788a10
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-11 23:15:49 -0400

    Update applicaton model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,4 @@
 class Application < ApplicationRecord
   validates_presence_of :pet_ids, :name, :address, :city, :state, :zip, :phone_number, :description
+  has_many :pets
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :description, :sex, :age, :adoptable
   belongs_to :shelter
+  has_many :pets
 end
