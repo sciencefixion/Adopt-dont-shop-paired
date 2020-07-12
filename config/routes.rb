cba364156e3b54dabc410024c7e6589521fbb039
Rion Hutsell <st.gish@gmail.com>
2020-07-11 19:10:33 -0600

    Add application form with checkboxes

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
   delete '/favorites', to: 'favorites#destroy_all'
 
   get '/applications/new', to: 'applications#new'
+  post '/applications', to: 'applications#create'
 end
