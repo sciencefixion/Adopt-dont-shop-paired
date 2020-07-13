53b1f38bfe05116e9013224938012c060e54eded
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-13 17:17:36 -0400

    Add spec to test that user can revoke an approved application Rebase master into revoke_app
    Rebase master into revoke_app

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -27,6 +27,9 @@ RSpec.describe 'Application show page', type: :feature do
       shelter: @shelter)
     @application = Application.create(name: 'Gabby', address: "24 Silver Street", city: "Springfield", state: "MA", zip: "01108", phone_number: "555-8987", description: "I'm a clown who needs a sidekick.")
     ApplicationPet.create(pet: @pet_1, application: @application)
+    @application_2 = Application.create(name: 'Hilal', address: "19 Gold Street", city: "Springfield", state: "MA", zip: "01108", phone_number: "555-8987", description: "I love all the pets.")
+    ApplicationPet.create(pet: @pet_2, application: @application_2)
+    ApplicationPet.create(pet: @pet_3, application: @application_2)
   end
   it 'shows an individual Application' do
     visit "/applications/#{@application.id}"
@@ -51,6 +54,7 @@ RSpec.describe 'Application show page', type: :feature do
     expect(page).to have_content("Adoption Status: pending")
     expect(page).to have_content("On hold for: Gabby")
   end
+<<<<<<< HEAD
 
   it "will not allow more than one approved applicant per pet" do
     #     As a visitor
@@ -60,6 +64,10 @@ RSpec.describe 'Application show page', type: :feature do
     # (This can be done by either taking away the option to approve the application, or having a flash message pop up saying that no more applications can be approved for this pet at this time)
     application_2 = Application.create(name: 'Crabby', address: "24 Sliver Street", city: "Springfield", state: "MA", zip: "01108", phone_number: "555-8789", description: "I'm a fish who needs a bicycle.")
     ApplicationPet.create(pet: @pet_1, application: application_2)
+=======
+  it 'can approve an application for more than one pet' do
+    visit "/applications/#{@application_2.id}"
+>>>>>>> 46f7dff... Add spec to test that user can revoke an approved application
 
     visit "/pets"
     click_on "Maggie"
@@ -75,5 +83,18 @@ RSpec.describe 'Application show page', type: :feature do
     click_on "Crabby"
     expect(page).to_not have_content("Approve Application for Maggie")
     expect(page).to have_content("#{@pet_1.name} is already on hold by another applicant.")
+  end
+  it 'can revoked an approved application' do
+    visit "/applications/#{@application_2.id}"
+
+    expect(page).to have_content("Revoke Application for #{@pet_2.name}")
+    click_on "Revoke Application for #{@pet_2.name}"
+
+    expect(current_path).to eq("/applications/#{@application_2.id}")
+    expect(page).to have_content("Approve Application for #{@pet_2.name}")
+
+    visit "/pets/#{@pet_2.id}"
+    expect(page).to have_content("Adoption Status: adoptable")
+    expect(page).to_not have_content("On hold for: Hilal")
   end
 end
