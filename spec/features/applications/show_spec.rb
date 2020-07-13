d2f1da7ea5878638062d7ca85839f29af12f3609
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-12 22:22:46 -0400

    Add spec for application approval for pet

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe 'Application show page', type: :feature do
-  it 'shows an individual Application' do
+  before(:each) do
     @shelter = Shelter.create(name: 'Old Dog Haven',
       address: '166 Main St',
       city: 'Denver',
@@ -19,17 +19,30 @@ RSpec.describe 'Application show page', type: :feature do
       age: '3 years',
       sex: 'male',
       shelter: @shelter)
-    application = Application.create(name: 'Gabby', address: "24 Silver Street", city: "Springfield", state: "MA", zip: "01108", phone_number: "555-8987", description: "I'm a clown who needs a sidekick.")
-    ApplicationPet.create(pet: @pet_1, application: application)
+    @application = Application.create(name: 'Gabby', address: "24 Silver Street", city: "Springfield", state: "MA", zip: "01108", phone_number: "555-8987", description: "I'm a clown who needs a sidekick.")
+    ApplicationPet.create(pet: @pet_1, @application: application)
+  end
+  it 'shows an individual Application' do
+    visit "/applications/#{@application.id}"
 
-    visit "/applications/#{application.id}"
+    expect(page).to have_content("#{@application.name}")
+    expect(page).to have_content("#{@application.description}")
+    expect(page).to have_content("#{@application.address}")
+    expect(page).to have_content("#{@application.city}")
+    expect(page).to have_content("#{@application.state}")
+    expect(page).to have_content("#{@application.zip}")
+    expect(page).to have_content("#{@application.phone_number}")
+  end
+  it 'can approve an application for a pet' do
+    visit "/applications/#{@application.id}"
 
-    expect(page).to have_content("#{application.name}")
-    expect(page).to have_content("#{application.description}")
-    expect(page).to have_content("#{application.address}")
-    expect(page).to have_content("#{application.city}")
-    expect(page).to have_content("#{application.state}")
-    expect(page).to have_content("#{application.zip}")
-    expect(page).to have_content("#{application.phone_number}")
+    expect(page).to have_content("Approve Application for #{@pet_1.name}")
+
+    click_on "Approve Application for #{@pet_1.name}"
+
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
+
+    expect(page).to have_content("Adoption Status: pending")
+    expect(page).to have_content("On hold for Gabby")
   end
 end
